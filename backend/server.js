3ccfed70c0144670cc639845d088d365e532e1c7
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-12-12 14:57:20 +0200

    refactor: Simplify CORS configuration by removing commented-out code

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,12 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-  
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",  
+    // credentials: true,
+  })
+);
 // app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
