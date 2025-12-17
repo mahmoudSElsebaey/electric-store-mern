4b903f86d7db9ddd77a1611985ca1df68b42c816
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-12-17 09:07:05 +0200

    `Update cookie settings in auth controller to depend on environment`

--- backend/src/modules/Auth/auth.controller.js
+++ backend/src/modules/Auth/auth.controller.js
@@ -13,8 +13,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true, // ← آمن ضد XSS
-      sameSite: "lax",
-      secure: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 أيام
     });
 
@@ -45,10 +45,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      // secure: process.env.NODE_ENV === "production",
-      // sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
