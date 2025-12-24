36d080a4a06c3210fb4c428df1c800b179d07036
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-12-24 14:36:49 +0200

    `Updated API base URL in frontend and backend code`

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
 // __________________________________ Error Handler _________________________________
 app.use((err, req, res, next) => {
